21ecc40e9abd8d11ba0b2a40e22790d8b4ba7471
1799566691451065.feishu.cn <1799566691451065@feishu.cn>
2024-07-31 07:52:03 +0000

    2 个文件产生了变更，影响了2 个 函数
    更新函数「node00001」
    更新函数「go01」

--- functions/nodejs/node00001/index.js
+++ functions/nodejs/node00001/index.js
@@ -12,6 +12,10 @@
 module.exports = async function (params, context, logger) {
   // 日志功能
    logger.info(`${new Date()} 函数开始执行`);
-
+   var faasenv = process.env.KFaaSType
+   logger.info(faasenv)
+   
+ 
+   
   // 在这里补充业务代码
 }
